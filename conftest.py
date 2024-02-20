106e3c264325d0c37f9c1f4dd7957843878cdcf9
VladimirMitienko <strykerjt@gmail.com>
2024-02-20 12:52:11 +0700

    Доработан confest.py

--- conftest.py
+++ conftest.py
@@ -2,9 +2,12 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture()
 def setting_browser():
     browser.config.window_height = 1080  # задает высоту окна браузера
     browser.config.window_width = 1920  # задает ширину окна браузера
+
+    browser.open("https://www.google.com/")
     yield
+
     browser.quit()
--- test.py
+++ test.py
@@ -2,18 +2,11 @@ from selene import browser, be, have
 import pytest
 
 
-@pytest.fixture
-def test_first(setting_browser):
-    browser.open("https://www.google.com/")
-
-
-def test_browser():
-    browser.open("https://www.google.com/")
+def test_browser(setting_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_invalid_search(setting_browser):
-    browser.open("https://www.google.com/")
-    browser.element('[name="q"]').should(be.blank).type('%$%^$#%#^%$^%@%@$@#$!%@#$%@#%%#%@%FDGdffgdsfgsdg').press_enter()
-    browser.element('[class="gL9Hy d2IKib"]').should(have.text('Возможно, вы имели в виду:'))
+    browser.element('[name="q"]').should(be.blank).type('77fgfgfgfg666566').press_enter()
+    browser.element('[class="v3jTId"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
